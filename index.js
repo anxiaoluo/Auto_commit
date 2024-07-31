4518ac09dbe4002e5c1eb799fb0be9d6837aa129
5u <39118854+anxiaoluo@users.noreply.github.com>
2024-07-31 10:52:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ export {execaCommand, execaCommandSync} from './lib/command.js';
 export {execaNode} from './lib/arguments/node.js';
 export {$} from './lib/script.js';
 export {ExecaError, ExecaSyncError} from './lib/return/cause.js';
+
+export {execa} from './lib/async.js';
+export {execaSync} from './lib/sync.js';
+export {execaCommand, execaCommandSync} from './lib/command.js';
+export {execaNode} from './lib/arguments/node.js';
+export {$} from './lib/script.js';
+export {ExecaError, ExecaSyncError} from './lib/return/cause.js';
